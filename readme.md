16b68741a914515319034e2ef3fd5c75df50611e
Serhii Lukianchuk <lukianchuk.s@gmail.com>
2024-10-03 21:43:52 -0400

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+
+Name:	Clinic Appointment Management Tool
+Team members: Serhii Lukianchuk, Adam Ashworth
+
+Description:	Tool that helps clerks in the clinic manage appointments for doctors.
+Functionality:
+
+Log in/ Log out
+Create visit
+Manage visits (change data, reschedule, delete)
+Filters by status/doctor/ urgency
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, '/src/index.js'),
+    },
+    output: {
+        filename: '[name][contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        assetModuleFilename: '[name][ext]',
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+        directory: path.resolve(__dirname, 'dist')
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+        {
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader',
+            ]
+        },
+        {
+            test: /\.js$/,
+            exclude: /node-modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        },
+        {
+            test:/\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'assets/resource'
+        }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Appointment Management Tool',
+        filename: 'index.html',
+        template: 'src/template.html',
+    })
+  ]
+};
