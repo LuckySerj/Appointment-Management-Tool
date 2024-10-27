3ef54fd4456cafa4c151bee3d4a08bbf442f2f1e
Serhii Lukianchuk <lukianchuk.s@gmail.com>
2024-10-27 17:54:51 -0400

    frontend deploy

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/main.scss';
-
-import Header from './components/header/header';
-import Filter from './components/filter/filter';
-
-const header = new Header();
-export { header };
-new Filter();
